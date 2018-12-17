35f7149e64b5779c6fa8b59fd76ba88d7729ca6a
Miriam B <bmiriam1230@gmail.com>
2018-12-17 17:06:35 -0600

    Chapter 1: Watching Object Properties: $watch And $digest
    $digest test:
    It calls the listener function of a watch on first $digest.
    
    Changes made to pass test:
    1. Added a $$watchers array to the scope via assignment in the Scope constructor function.
    2. Defined the $watch function. Added it to the prototype so every Scope object inherits this function. $watch function accepts watchFn and listenerFn, creates a watcher object with both watchFn and listenerFn as methods, and then pushes to $$watchers array.
    3. Created a $digest function which iterates over watchers and calls listenerFn. This is a very naive implementation of $digest for far because it does not check if the value returned by the watchFn has changed as a condition for calling the listenerFn.

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -10,3 +10,22 @@ describe('Scope', function() {
     expect(scope.aProperty).toBe(1);
   });
 });
+
+describe('digest', function() {
+  var scope;
+
+  beforeEach(function() {
+    scope = new Scope();
+  });
+
+  it('calls the listener function of a watch on first $digest', function() {
+    var listenerFnHasRun = false;
+    var watchFn = function() { return 'wat'; };
+    var listenerFn = function() { listenerFnHasRun = true; };
+
+    scope.$watch(watchFn, listenerFn);
+    scope.$digest();
+
+    expect(listenerFnHasRun).toBe(true);
+  });
+});
