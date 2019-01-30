d78019143f5a266ce46a6c1ab006493b867647d9
Miriam B <bmiriam1230@gmail.com>
2019-01-30 14:49:21 -0600

    Chapter 4: Detecting New Objects
    Adding The Following Test:
    -Notices when the value becomes an object.

--- src/scope.js
+++ src/scope.js
@@ -110,7 +110,10 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
           }
         });
       } else {
-
+        if (!_.isObject(oldValue) || isArrayLike(oldValue)) {
+          changeCount++;
+          oldValue = {};
+        }
       }
     } else {
       if (!self.$$areEqual(newValue, oldValue, false)) {
