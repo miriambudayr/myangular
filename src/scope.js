b433bbc51dcf5691a9b6ceea8a7231b70246454c
Miriam B <bmiriam1230@gmail.com>
2019-02-05 12:46:05 -0600

    Chapter 4: Dealing With Objects That Have A Length
    Adding the Following Test:
    -It does not consider any object with a length property an array.

--- src/scope.js
+++ src/scope.js
@@ -352,7 +352,8 @@ function isArrayLike(obj) {
     return false;
   }
   length = obj.length;
-  return _.isNumber(length);
+  return length === 0 ||
+    (_.isNumber(length) && length > 0 && (length - 1) in obj);
 }
 
 Scope.prototype.$$everyScope = function(fn) {
